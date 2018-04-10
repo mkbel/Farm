4186ccf24b29136645400225d1955365bdedcc55
Marek Bel <marek.bel@posta.cz>
2018-04-10 16:28:42 +0200

    Trigger sending by 's' character reception. Workaround: Remove infinite loop to be able exit script by ctrl+c.

--- onInit.m
+++ onInit.m
@@ -1,12 +1,10 @@
+#!/usr/bin/octave -f
 pkg load instrument-control
 s1 = serial("/dev/ttyUSB0", 115200)
 srl_flush(s1)
 delay = 0.22
-
-while 1
+do 
   data = srl_read(s1, 1)
-  if data == "s"
-    sleep(delay)
-    srl_write(s1, "PRUSA thx\n")
-  endif
-endwhile
+until ('s' == data(1))
+pause(delay)
+srl_write(s1, "PRUSA thx\n")
