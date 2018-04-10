0cfcec0a2748d4a17d4eef06816c20f58169d94a
Marek Bel <marek.bel@posta.cz>
2018-04-10 18:13:38 +0200

    Uspesne vybuzeno na farme.

--- onInit.m
+++ onInit.m
@@ -1,10 +1,18 @@
 #!/usr/bin/octave -f
 pkg load instrument-control
-s1 = serial("/dev/ttyUSB0", 115200)
+s1 = serial("/dev/ttyUSB1", 115200)
 srl_flush(s1)
-delay = 0.22
+lag = 0.005
+delay = 0.23
+
 do 
   data = srl_read(s1, 1)
 until ('s' == data(1))
-pause(delay)
+# pause(delay-lag)
+#srl_write(s1, "PRUSA thx\n")
+# pause(1.001)
+# pause(1.033)
+pause(1.0)
+srl_write(s1, "PRUSA SN\n")
+pause(3.0)
 srl_write(s1, "PRUSA thx\n")
